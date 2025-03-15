5cbdf9911fac8ff34329772e3e9b568b89532fe5
s0ykot <soykkot.ch@gmail.com>
2025-03-15 06:07:32 +0600

    verion 1.1

--- UHR.py
+++ UHR.py
@@ -1,82 +1,90 @@
 from burp import IBurpExtender, IHttpListener, IMessageEditorTabFactory, IMessageEditorTab
-from javax.swing import JTextArea, JScrollPane
-from java.awt.datatransfer import StringSelection
-from java.awt import Toolkit
-from java.io import PrintWriter
-import hashlib
+from javax.swing import JTextArea
+import re
 
 class BurpExtender(IBurpExtender, IHttpListener, IMessageEditorTabFactory):
-
+    
     def registerExtenderCallbacks(self, callbacks):
         self._callbacks = callbacks
         self._helpers = callbacks.getHelpers()
-        callbacks.setExtensionName("Unnecessary Header Remove")
-        self.stdout = PrintWriter(callbacks.getStdout(), True)
-        self.stderr = PrintWriter(callbacks.getStderr(), True)
-        self.cleaned_requests = {}
-        callbacks.registerMessageEditorTabFactory(self)
+        
+        callbacks.setExtensionName("Header Cleaner")
+        
+        # Register HTTP listener
         callbacks.registerHttpListener(self)
-        self.stdout.println("[+] Header Cleaner Plugin Loaded Successfully!")
-
+        
+        # Register tab factory
+        callbacks.registerMessageEditorTabFactory(self)
+    
     def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
-        """ Cleans the HTTP request by removing unnecessary headers """
         if messageIsRequest:
-            request = messageInfo.getRequest()
-            request_info = self._helpers.analyzeRequest(request)
-            headers = request_info.getHeaders()
-            body = request[request_info.getBodyOffset():]
-            # Headers to remove
-            uninteresting_headers = {
-                "Accept",
-                "Accept-Language",
-                "Accept-Encoding",
-                "Upgrade-Insecure-Requests",
-                "Sec-Fetch-Dest",
-                "Sec-Fetch-Mode",
-                "Sec-Fetch-Site",
-                "Sec-Fetch-User",
-                "Priority"
-            }
-            filtered_headers = [header for header in headers if not any(header.lower().startswith(h.lower()) for h in uninteresting_headers)]
-            cleaned_request = "\n".join(filtered_headers) + "\n\n" + self._helpers.bytesToString(body)
-            # Store cleaned request in dictionary with hash of request as key
-            request_hash = hashlib.md5(request).hexdigest()
-            self.cleaned_requests[request_hash] = cleaned_request
-
+            request_info = self._helpers.analyzeRequest(messageInfo)
+            headers = list(request_info.getHeaders())
+            body = messageInfo.getRequest()[request_info.getBodyOffset():]
+            
+            headers_to_remove = set([
+                "accept", "accept-encoding", "accept-language", "if-modified-since", "if-none-match", "priority",
+                "sec-ch-ua", "sec-ch-ua-arch", "sec-ch-ua-bitness", "sec-ch-ua-full-version", "sec-ch-ua-mobile", "sec-ch-ua-model", 
+                "sec-ch-ua-platform", "sec-ch-ua-platform-version", "sec-ch-ua-wow64", "sec-fetch-dest", "sec-fetch-mode", 
+                "sec-fetch-site", "sec-fetch-user", "upgrade-insecure-requests", "x-requested-with"
+            ])
+            
+            cleaned_headers = [headers[0]]  # Keep request line (e.g., GET / HTTP/1.1)
+            for header in headers[1:]:
+                header_name = header.split(":")[0].strip().lower()
+                if header_name not in headers_to_remove:
+                    cleaned_headers.append(header)
+            
+            new_request = self._helpers.buildHttpMessage(cleaned_headers, body)
+            messageInfo.setRequest(new_request)
+    
     def createNewInstance(self, controller, editable):
-        return CleanedRequestTab(self._helpers, controller, self.cleaned_requests)
+        return CleanedHeadersTab(self._callbacks, self._helpers, controller, editable)
 
-
-class CleanedRequestTab(IMessageEditorTab):
-
-    def __init__(self, helpers, controller, cleaned_requests):
+class CleanedHeadersTab(IMessageEditorTab):
+    def __init__(self, callbacks, helpers, controller, editable):
         self._helpers = helpers
         self._controller = controller
-        self.cleaned_requests = cleaned_requests  # Dictionary of cleaned requests
-        self.text_area = JTextArea()
-        self.text_area.setEditable(False)
-        self.scroll_pane = JScrollPane(self.text_area)
-
+        self._editable = editable
+        self._txtArea = JTextArea()
+        self._txtArea.setEditable(False)
+    
     def getTabCaption(self):
-        return "Cleaned Request"
-
+        return "Cleaned Headers"
+    
     def getUiComponent(self):
-        return self.scroll_pane
-
+        return self._txtArea
+    
     def isEnabled(self, content, isRequest):
         return isRequest
-
+    
     def setMessage(self, content, isRequest):
         if isRequest:
-            request_hash = hashlib.md5(content).hexdigest()
-            cleaned_request = self.cleaned_requests.get(request_hash, "No cleaned request available.")
-            self.text_area.setText(cleaned_request)
-
+            request_info = self._helpers.analyzeRequest(content)
+            headers = list(request_info.getHeaders())
+            
+            headers_to_remove = set([
+                "accept", "accept-encoding", "accept-language", "if-modified-since", "if-none-match", "priority",
+                "sec-ch-ua", "sec-ch-ua-arch", "sec-ch-ua-bitness", "sec-ch-ua-full-version", "sec-ch-ua-mobile", "sec-ch-ua-model", 
+                "sec-ch-ua-platform", "sec-ch-ua-platform-version", "sec-ch-ua-wow64", "sec-fetch-dest", "sec-fetch-mode", 
+                "sec-fetch-site", "sec-fetch-user", "upgrade-insecure-requests", "x-requested-with"
+            ])
+            
+            cleaned_headers = [headers[0]]  # Keep request line (e.g., GET / HTTP/1.1)
+            for header in headers[1:]:
+                header_name = header.split(":")[0].strip().lower()
+                if header_name not in headers_to_remove:
+                    cleaned_headers.append(header)
+            
+            self._txtArea.setText("\n".join(cleaned_headers))
+        else:
+            self._txtArea.setText("")
+    
     def getMessage(self):
         return None
-
+    
     def isModified(self):
         return False
-
+    
     def getSelectedData(self):
-        return self.text_area.getSelectedText()
+        return None
